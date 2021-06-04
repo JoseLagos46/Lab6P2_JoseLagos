386f4629577b0187f1a204b3c83d68750b5ccbfc
JoseLagos <josealagos46@gmail.com>
2021-06-04 13:36:00 -0600

    Creacion de proyecto y clases

---
+++ Lab6P2_JoseLagos/src/lab6p2_joselagos/Lab6P2_JoseLagos.java
@@ -0,0 +1,12 @@
+package lab6p2_joselagos;
+
+public class Lab6P2_JoseLagos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
